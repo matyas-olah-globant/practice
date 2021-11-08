78191547891f3ba9be1d06b2a9aa33597da1ee66
matyas-olah-globant <83953852+matyas-olah-globant@users.noreply.github.com>
2021-11-08 13:52:35 +0200

    sout change reverted

--- src/org/matyas94k/example/Main.java
+++ src/org/matyas94k/example/Main.java
@@ -2,7 +2,7 @@ package org.matyas94k.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Changed online, on GitHub, then locally, in IDE.");
+        System.out.println("Changed back.");
     }
 
 }
