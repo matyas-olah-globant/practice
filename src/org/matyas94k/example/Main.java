9ae71d8e016606fb8eb00e4293f0da41d957ee26
Mátyás Oláh <matyas.olah@globant.com>
2021-11-08 13:56:10 +0200

    new sout

--- src/org/matyas94k/example/Main.java
+++ src/org/matyas94k/example/Main.java
@@ -3,6 +3,7 @@ package org.matyas94k.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Changed back.");
+        System.out.printf("%s%n", Math.PI);
     }
 
 }
